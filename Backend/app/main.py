b725e3fff6624039d336a4c523a3849f734b1693
David Elbaz <102173191+DavidELBAZpro@users.noreply.github.com>
2025-07-01 03:31:13 +0200

    Refactor main.py to implement async lifespan management and improve app setup structure

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -3,6 +3,8 @@ from logging.handlers import TimedRotatingFileHandler
 from os.path import join
 import sys
 from pathlib import Path
+from contextlib import asynccontextmanager
+
 
 # Append root dir path to the sys path so that imports are correctly working no matters from where the app is run.
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
@@ -15,9 +17,6 @@ import settings as app_settings
 from database import create_db_and_tables
 from routers.default import router as default_router
 from routers.ticket import router as ticket_router
-app = FastAPI()
-app.include_router(default_router)
-app.include_router(ticket_router)
 
 
 def configure_logging(console_handler: bool = False):
@@ -54,6 +53,18 @@ def setup_app():
     configure_logging(True)
     create_db_and_tables()
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    setup_app()
+    yield
+ 
+
+
+app = FastAPI(lifespan=lifespan)
+
+# Routes
+app.include_router(default_router)
+app.include_router(ticket_router)
 
 if __name__ == "__main__":
     setup_app()
